efbc1265ada5532826fd9c14351547af5bd39221
Haiaio <68382117+Haiaio@users.noreply.github.com>
2025-01-28 20:08:15 +0100

    Mas plataforma movil

--- Assets/Plataforma Movil.cs
+++ Assets/Plataforma Movil.cs
@@ -1,10 +1,10 @@
 using UnityEngine;
 
-public class PlataformaUp : MonoBehaviour
+public class PlataformaMovil : MonoBehaviour
 {
     public Vector3 puntoA;
     public Vector3 puntoB;
-    public float velocidad = 2f;
+    public float velocidad = 3f;
     private bool moviendoHaciaB = true;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -21,12 +21,34 @@ public class PlataformaUp : MonoBehaviour
                 moviendoHaciaB = false;
             }
         }
-       /* else
+        else
         {
             transform.position = Vector3.MoveTowards(transform.position,puntoA,velocidad*Time.deltaTime);
             if(transform.position == puntoA){
                 moviendoHaciaB = true;
             }
-        }*/
+        }
+        
+    }  
+
+
+       private void OnCollisionEnter(Collision collision)
+    {
+        // Check if the player collided with the platform
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            // Make the player a child of the platform
+            collision.transform.SetParent(transform,true);
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        // Check if the player left the platform
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            // Detach the player from the platform
+            collision.transform.SetParent(null);
+        }
     }
 }
